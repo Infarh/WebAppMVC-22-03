1a6ded152dc8a033767c9e923df4f7f480688760
Infarh <shmachilin@yandex.ru>
2022-03-21 19:48:34 +0300

    Подготовка к многопоточности

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+//var current_thread = Thread.CurrentThread;
+
+//current_thread.Priority = ThreadPriority.Lowest;
+
+
+while (true)
+{
+    Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff");
+}
+
+
+Console.WriteLine("Программа ожидает ввода пользователя");
+
+Console.ReadLine();
+
+
+Console.WriteLine("Программа завершена");
