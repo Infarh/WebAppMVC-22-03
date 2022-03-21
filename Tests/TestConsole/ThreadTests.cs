aa6b48181a6f4052eda9b93906900127b5fbb431
Infarh <shmachilin@yandex.ru>
2022-03-21 20:06:36 +0300

    Прерывание потока с помощью thread.Interrupt()

--- Tests/TestConsole/ThreadTests.cs
+++ Tests/TestConsole/ThreadTests.cs
@@ -5,23 +5,42 @@ internal static class ThreadTests
     public static void Run()
     {
         var timer_thread = new Thread(TimerUpdate);
+        timer_thread.Priority = ThreadPriority.BelowNormal;
+        timer_thread.Name = "Поток обновления часов в заголовке консоли";
+        timer_thread.IsBackground = true;
         timer_thread.Start();
 
         Console.WriteLine("Программа ожидает ввода пользователя");
 
         Console.ReadLine();
 
+        if (!timer_thread.Join(300))
+        {
+            timer_thread.Interrupt();
+            timer_thread.Join();
+        }
 
         Console.WriteLine("Программа завершена");
     }
 
     private static void TimerUpdate()
     {
-        while (true)
+        try
         {
-            Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff");
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff");
 
-            Thread.Sleep(250);
+                Thread.Sleep(250);
+            }
+        }
+        catch (ThreadInterruptedException)
+        {
+            Console.WriteLine("Поток прерван \"мягко\"");
+        }
+        catch (ThreadAbortException)
+        {
+            Console.WriteLine("Поток прерван \"жёстко\"");
         }
     }
 }
