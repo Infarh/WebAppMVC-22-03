02a262abb0c1b0e27de5910b6c36b9a94cc84fae
Infarh <shmachilin@yandex.ru>
2022-03-21 19:53:08 +0300

    Рефакторинг

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,21 +1,9 @@
 ﻿using System.Threading;
+using TestConsole;
 
 //var current_thread = Thread.CurrentThread;
 
 //current_thread.Priority = ThreadPriority.Lowest;
 
 
-while (true)
-{
-    Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff");
-
-    Thread.Sleep(250);
-}
-
-
-Console.WriteLine("Программа ожидает ввода пользователя");
-
-Console.ReadLine();
-
-
-Console.WriteLine("Программа завершена");
+ThreadTests.Run();
---
+++ Tests/TestConsole/ThreadTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TestConsole;
+
+internal static class ThreadTests
+{
+    public static void Run()
+    {
+        while (true)
+        {
+            Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff");
+
+            Thread.Sleep(250);
+        }
+
+
+        Console.WriteLine("Программа ожидает ввода пользователя");
+
+        Console.ReadLine();
+
+
+        Console.WriteLine("Программа завершена");
+    }
+}
