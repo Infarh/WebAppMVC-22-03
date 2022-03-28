7e4669cee0af7c49b5e5e7cd36f0988b5d054919
Infarh <shmachilin@yandex.ru>
2022-03-28 20:57:25 +0300

    Построитель графиков функций

--- Tests/TestConsole/BuilderPatternExample.cs
+++ Tests/TestConsole/BuilderPatternExample.cs
@@ -49,3 +49,59 @@ public class BuilderPatternExample
             exporter.Export(model, png);
     }
 }
+
+public class PlotBuilder
+{
+    private readonly List<(Func<double, double> f, double MinX, double MaxX, double dx, OxyColor Color, double StrokeThickness)> _Plots = new();
+
+    public OxyColor Background { get; set; }
+
+    public OxyColor AxisLeftMajorGridlineColor { get; set; }
+    public OxyColor AxisBottomMajorGridlineColor { get; set; }
+
+    public LineStyle AxisLeftMajorGridlineStyle { get; set; }
+    public LineStyle AxisBottomMajorGridlineStyle { get; set; }
+
+    public OxyColor AxisLeftMinorGridlineColor { get; set; }
+    public OxyColor AxisBottomMinorGridlineColor { get; set; }
+
+    public LineStyle AxisLeftMinorGridlineStyle { get; set; }
+    public LineStyle AxisBottomMinorGridlineStyle { get; set; }
+
+    public void Plot(Func<double, double> f, double MinX, double MaxX, double dx, OxyColor Color, double StrokeThickness)
+    {
+        _Plots.Add((f, MinX, MaxX, dx, Color, StrokeThickness));
+    }
+
+    public PlotModel CreateModel()
+    {
+        var model = new PlotModel
+        {
+            Axes =
+            {
+                new LinearAxis
+                {
+                    Position = AxisPosition.Left,
+                    MajorGridlineColor = AxisLeftMajorGridlineColor,
+                    MajorGridlineStyle = AxisLeftMajorGridlineStyle,
+                    MinorGridlineColor = AxisLeftMinorGridlineColor,
+                    MinorGridlineStyle = AxisLeftMinorGridlineStyle,
+                },
+                new LinearAxis
+                {
+                    Position = AxisPosition.Bottom,
+                    MajorGridlineColor = AxisBottomMajorGridlineColor,
+                    MajorGridlineStyle = AxisBottomMajorGridlineStyle,
+                    MinorGridlineColor = AxisBottomMinorGridlineColor,
+                    MinorGridlineStyle = AxisBottomMinorGridlineStyle,
+                },
+            },
+            Background = Background,
+        };
+
+        foreach (var (f, min, max, dx, color, thicknes) in _Plots)
+            model.Series.Add(new FunctionSeries(f, min, max, dx) { Color = color, StrokeThickness = thicknes });
+
+        return model;
+    }
+}
