2d2cc4008a63e5d555fe6343bfa13f8cc9d8a694
Infarh <shmachilin@yandex.ru>
2022-03-31 19:43:19 +0300

    XML-сериализация студентов

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,6 +1,41 @@
 ﻿using TestConsole;
+using System.Xml.Serialization;
 
-//LoggingExamples.Run();
-BuilderPatternExample.Run();
+var student = new Student
+{
+    Id = 1,
+    LastName = "Иванов",
+    FirstName = "Иван",
+    Patronymic = "Иванович",
+    Birthday = DateTime.Now.AddYears(-18)
+};
+
+var xml_serializer = new XmlSerializer(typeof(Student));
+
+const string xml_file_name = "student.xml";
+
+using (StreamWriter xml_file = File.CreateText(xml_file_name))
+{
+    xml_serializer.Serialize(xml_file, student);
+}
+
+//{
+//    var xml_file = File.CreateText(xml_file_name);
+//    try
+//    {
+//        xml_serializer.Serialize(xml_file, student);
+//    }
+//    finally
+//    {
+//        xml_file.Dispose();
+//    }
+//}
+
+
+Student? student2 = null;
+using (var xml_file = File.OpenText(xml_file_name))
+{
+    student2 = (Student?)xml_serializer.Deserialize(xml_file);
+}
 
 Console.ReadLine();
---
+++ Tests/TestConsole/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace TestConsole;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    public string LastName { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string Patronymic { get; set; }
+    
+    public DateTime Birthday { get; set; }
+}
