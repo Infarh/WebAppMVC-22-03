f067310d57cde70cac55dcf72d6a38a8b6ed5379
Infarh <shmachilin@yandex.ru>
2022-04-07 21:21:54 +0300

    Конфигурация контейнера Autofac

---
+++ WebAppMVC/Infrastructure/Autofac/ServicesModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using WebAppMVC.Services;
+using WebAppMVC.Services.Interfaces;
+
+namespace WebAppMVC.Infrastructure.Autofac;
+
+public class ServicesModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+
+        builder.RegisterType<SqlOrderService>()
+           .As<IOrderService>()
+           .InstancePerLifetimeScope();
+
+        builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+           .Where(type => type.Namespace!.EndsWith("Services"))
+           .AsImplementedInterfaces();
+    }
+}
