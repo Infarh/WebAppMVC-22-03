838f2089a1309bbf3c71ce3ee8b15d88a67b34b3
Infarh <shmachilin@yandex.ru>
2022-04-07 20:58:55 +0300

    Сервис  заказов

---
+++ WebAppMVC/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿using Orders.DAL.Entities;
+
+namespace WebAppMVC.Services.Interfaces;
+
+public interface IOrderService
+{
+    Task<Order> CreateAsync(string BuyerName, string Address, string Phone, IEnumerable<(Product Product, int Quantity)> Products);
+}
