d1341d8ea1d4dd11d2a81408aec41689fa031beb
Infarh <shmachilin@yandex.ru>
2022-04-07 20:45:21 +0300

    Добавление контекста БД в контейнер сервисов MVC-приложения

--- WebAppMVC/Program.cs
+++ WebAppMVC/Program.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.EntityFrameworkCore;
+using Orders.DAL.Context;
 using WebAppMVC.Infrastructure.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Конфигурация сервисов приложения
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddDbContext<OrdersDB>(opt => opt.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+#endregion
+
 var app = builder.Build();
+
+// настройка конвейера обработки запросов
 
 app.UseMiddleware<TestMiddleware>();
 
