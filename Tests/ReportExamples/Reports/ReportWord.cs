63bf6ce65c25701ef4aa853bda22ba6c592f05ae
Infarh <shmachilin@yandex.ru>
2022-04-11 19:35:20 +0300

    Заготовка генератора отчёта в Word

---
+++ Tests/ReportExamples/Reports/ReportWord.cs
@@ -0,0 +1,17 @@
+﻿using ReportExamples.Interfaces;
+
+namespace ReportExamples.Reports;
+
+public class ReportWord : IProductsReport
+{
+    public string CatalogName { get; set; } = null!;
+
+    public DateTime CreationDate { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public FileInfo Create(string ReportTemplateFile)
+    {
+        throw new NotImplementedException();
+    }
+}
