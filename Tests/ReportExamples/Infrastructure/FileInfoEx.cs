85f481dbe17334399c30d7180da8d391bacf333e
Infarh <shmachilin@yandex.ru>
2022-04-11 19:39:36 +0300

    Метод-расширение для FileInfo для обеспечения возможности выполнения файлов в ОС.

---
+++ Tests/ReportExamples/Infrastructure/FileInfoEx.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Diagnostics;
+
+internal static class FileInfoEx
+{
+    public static Process? Execute(this FileInfo file, bool UseShellExecute = true)
+    {
+        var info = new ProcessStartInfo(file.FullName) { UseShellExecute = UseShellExecute };
+        return Process.Start(info);
+    }
+}
