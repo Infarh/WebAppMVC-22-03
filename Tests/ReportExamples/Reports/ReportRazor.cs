d542fa25face446ba39a01bcededc73bc3a75712
Infarh <shmachilin@yandex.ru>
2022-04-11 20:36:23 +0300

    Заготовка для генератора документа на основе движка Razor

---
+++ Tests/ReportExamples/Reports/ReportRazor.cs
@@ -0,0 +1,25 @@
+﻿using ReportExamples.Interfaces;
+
+namespace ReportExamples.Reports;
+
+public class ReportRazor : IProductsReport
+{
+    public string CatalogName { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public string Description { get; set; }
+
+    public IEnumerable<(int Id, string Name, string Description, decimal Price)> Products { get; set; }
+
+    public FileInfo Create(string ReportFilePath)
+    {
+        var report_file = new FileInfo(ReportFilePath);
+        report_file.Delete();
+
+
+
+        report_file.Refresh();
+        return report_file;
+    }
+}
