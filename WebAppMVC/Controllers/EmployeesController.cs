71fd3a00963a4b03ce2e0eb5ba2a5e71eb4acc1f
Infarh <shmachilin@yandex.ru>
2022-04-14 21:01:34 +0300

    Сотрудники отправлены в отдельный контроллер

---
+++ WebAppMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebAppMVC.Services.Interfaces;
+
+namespace WebAppMVC.Controllers;
+
+public class EmployeesController : Controller
+{
+    private readonly IEmployeesStore _EmployeesStore;
+    private readonly ILogger<EmployeesController> _Logger;
+
+    public EmployeesController(IEmployeesStore EmployeesStore, ILogger<EmployeesController> Logger)
+    {
+        _EmployeesStore = EmployeesStore;
+        _Logger = Logger;
+    }
+
+    public IActionResult Index()
+    {
+        var employees = _EmployeesStore.GetAll();
+
+        return View(employees);
+    }
+}
--- WebAppMVC/Controllers/HomeController.cs
+++ WebAppMVC/Controllers/HomeController.cs
@@ -6,21 +6,17 @@ namespace WebAppMVC.Controllers;
 //[Controller]
 public class HomeController : Controller
 {
-    private readonly IEmployeesStore _EmployeesStore;
     private readonly ILogger<HomeController> _Logger;
 
     public HomeController(IEmployeesStore EmployeesStore, ILogger<HomeController> Logger)
     {
-        _EmployeesStore = EmployeesStore;
         _Logger = Logger;
     }
 
     public IActionResult Index()
     {
-        var employees = _EmployeesStore.GetAll();
-
         //ViewBag.Title = "123";
         //var title = ViewData["Title"];
-        return View(employees);
+        return View();
     }
 }
