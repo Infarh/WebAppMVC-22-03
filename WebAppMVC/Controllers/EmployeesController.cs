358f4c281b7c3df90ca31916e597cfb15330b33b
Infarh <shmachilin@yandex.ru>
2022-04-14 21:15:46 +0300

    Редактирование сотрудника

--- WebAppMVC/Controllers/EmployeesController.cs
+++ WebAppMVC/Controllers/EmployeesController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using WebAppMVC.Models;
 using WebAppMVC.Services.Interfaces;
 using WebAppMVC.ViewModels;
 
@@ -41,7 +41,41 @@ public class EmployeesController : Controller
 
     public IActionResult Create() => View();
 
-    public IActionResult Edit(int id) => View();
+    public IActionResult Edit(int id)
+    {
+        var employee = _EmployeesStore.GetById(id);
+        if (employee is null)
+            return NotFound();
+
+        return View(new EmployeesViewModel
+        {
+            Id = employee.Id,
+            LastName = employee.LastName,
+            FirstName = employee.FirstName,
+            Patronymic = employee.Patronymic,
+            Birthday = employee.Birthday,
+        });
+    }
+
+    [HttpPost]
+    public IActionResult Edit(EmployeesViewModel Model)
+    {
+        var employee = new Employee
+        {
+            Id = Model.Id,
+            LastName = Model.LastName,
+            FirstName = Model.FirstName,
+            Patronymic = Model.Patronymic,
+            Birthday = Model.Birthday,
+        };
+
+        var success = _EmployeesStore.Edit(employee);
+
+        if (!success)
+            return NotFound();
+
+        return RedirectToAction("Index");
+    }
 
     public IActionResult Delete(int id) => View();
 }
