69c4167bcac7b5ae279694b2f8544e0ca70cd308
Infarh <shmachilin@yandex.ru>
2022-04-18 20:29:14 +0300

    Заготовка контроллера Account

---
+++ WebAppMVC/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Identity.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<User> _User;
+    private readonly SignInManager<User> _SignInManager;
+    private readonly ILogger<AccountController> _Logger;
+
+    public AccountController(
+        UserManager<User> User, 
+        SignInManager<User> SignInManager,
+        ILogger<AccountController> Logger)
+    {
+        _User = User;
+        _SignInManager = SignInManager;
+        _Logger = Logger;
+    }
+
+    public IActionResult Register() => View();
+    
+    public IActionResult Login() => View();
+    
+    public IActionResult Logout() => View();
+
+    public IActionResult AccessDenied() => View();
+}
