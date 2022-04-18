f7fe90ef912815c4ea205e0f834a546afef6a256
Infarh <shmachilin@yandex.ru>
2022-04-18 20:34:37 +0300

    Модели-представлений контроллера Account

---
+++ WebAppMVC/ViewModels/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMVC.ViewModels.Identity;
+
+public class RegisterUserViewModel
+{
+    [Required]
+    [Display(Name = "Имя пользователя")]
+    public string UserName { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Подтверждение пароля")]
+    [Compare(nameof(Password), ErrorMessage = "Пароли не совпадают")]
+    public string PasswordConfirm { get; set; } = null!;
+}
